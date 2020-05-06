9f8e7aad8b6be13d236f8ce4aa13c5cde460ac8c
Binup karki <karki011@gmail.com>
2020-05-06 10:18:08 -0500

    updated path

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,6 +5,6 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('recipe/<int:recipe_id>', views.recipe_detail, name="recipe_detail"),
-    path('author/<int:author_id>', views.author_detail, name="authorDetail")
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name="recipe_detail"),
+    path('author/<int:author_id>/', views.author_detail, name="authorDetail")
 ]
