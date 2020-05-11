b99d2119bdeb76db8d653c482ec65434b0a34d4a
Binup karki <karki011@gmail.com>
2020-05-10 22:04:32 -0500

    added forgot password

--- receipes_box/settings.py
+++ receipes_box/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'receipes_box.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -80,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -100,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -114,7 +110,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -123,3 +118,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'receipes_box/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+# SMTP config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mailtositar@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sub@sh123@'
--- recipe/urls.py
+++ recipe/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 app_name = 'recipes'
@@ -18,5 +20,17 @@ urlpatterns = [
     path('signup/', views.register_view, name='register_page'),
     path('userpage/', views.userpage, name='userpage'),
     path('denied/', views.accessdenied, name='denied'),
+
+    path('change-password/', auth_views.PasswordChangeView.as_view()),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+
+    path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name="password_reset_complete"),
 
 ]
