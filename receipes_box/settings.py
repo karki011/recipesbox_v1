8156add1c8e85c3dd175ff27772c0bf74e347b5b
Binup karki <karki011@gmail.com>
2020-05-17 22:23:54 -0500

    minor change

--- receipes_box/settings.py
+++ receipes_box/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # Activate Django-Heroku.
 django_heroku.settings(locals())
